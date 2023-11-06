29e37b3b2658637b4777f39a856a6854089029d4
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-05 21:59:24 -0500

    Added routes, file is small enough to be condensed to server.js so extraneous files were removed

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 const express = require('express');
-const api_routes = require('./routes/api_routes');
 const path = require('path');
+const fs = require('fs');
+
+const db = path.join(__dirname, './db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3333;
+
+//HTML Routes
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.get('/notes', (req, res) => {
     
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
+
+//API Routes
+
+const router = express.Router();
+
+router.route('/api/notes')
+    .get((req, res) => {
+        const data = fs.readFileSync(db, 'utf-8');
+        res.json(JSON.parse(data));
+    })  
+    .post((req, res) => {
+        const storedData = JSON.parse(fs.readFileSync(db, 'utf-8'));
+        const newData = {
+            title: req.body.title,
+            text: req.body.text
+        }
+        storedData.push(newData);
+        fs.writeFile(db, JSON.stringify(storedData, null, 4), () => {
+            console.log ('Added new note.')
+        })
+    })
 
 
 app.listen(PORT, () => console.log('Server started on %s', PORT));
-
-
